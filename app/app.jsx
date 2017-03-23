a0cd8f48d0199ee9d2e46586e8040f9cbed01d23
Leah Wolf <ycleahwolf@gmail.com>
2017-03-23 11:36:55 +0200

    Added copy

--- app/app.jsx
+++ app/app.jsx
@@ -1,16 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-
-// var Route = require ('react-router').Route;
-// var Router = require ('react-router').Router;
-// var IndexRoute = require ('react-router').IndexRoute;
-// var hashHistory = require ('react-router').hashHistory;
-
 var Main = require('Main');
 var Weather = require('Weather');
 var About = require("About");
 var Examples = require("Examples");
+
+// Load foundations
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
 
 ReactDOM.render(
   <Router history={hashHistory}>
